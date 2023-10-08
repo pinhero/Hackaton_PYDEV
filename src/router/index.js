b9a2309c30a3b7326060b396eb6249fc4d3a08dd
baekexsoo <ndamarina17@gmail.com>
2023-10-08 17:00:04 +0100

    My commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: () =>
+        import('../views/LoginView.vue')
+    },
     // {
     //   path: '/about',
     //   name: 'about',
@@ -24,7 +25,7 @@ const router = createRouter({
         import('../views/RegisterView.vue')
     },
     {
-      path: '/',
+      path: '/login',
       name: 'login',
       component: () =>
         import('../views/LoginView.vue')
