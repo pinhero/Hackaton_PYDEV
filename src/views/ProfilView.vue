03e12d08406ae2fcdc895afbb43303313e57f249
baekexsoo <ndamarina17@gmail.com>
2023-10-08 16:20:40 +0100

    My commit

---
+++ src/views/ProfilView.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="  max-w-2xl mx-auto">
+        <div class="page__heading py-6 mt-6">
+
+            <h1 class="font-bold text-3xl"> Scientific profile </h1>
+        </div>
+        <div class="mt-6 mb-20 text-sm font-medium text-gray-600 dark:text-white/80">
+
+            <div
+                class="bg-white border rounded-xl shadow-sm md:py-12 md:px-20 p-6 overflow-hidden dark:border-slate-700 dark:bg-dark2">
+
+                <div class="space-y-6">
+
+                    <div class="md:flex items-center gap-10">
+                        <label class="md:w-32 text-right"> Academic education </label>
+                        <div class="flex-1 max-md:mt-4">
+                            <input type="text" placeholder="Your formation"
+                                class="w-full h-10 border-gray-400 border-2 rounded-md p-1 ">
+                        </div>
+                    </div>
+
+                    <div class="md:flex items-center gap-10">
+                        <label class="md:w-32 text-right"> Expertise </label>
+                        <div class="flex-1 max-md:mt-4">
+                            <input type="text" placeholder="Your expertise"
+                                class="w-full h-10 border-gray-400 border-2 rounded-md p-1">
+                        </div>
+                    </div>
+
+                    <div class="md:flex items-center gap-10">
+                        <label class="md:w-32 text-right"> Scientific Method </label>
+                        <div class="flex-1 max-md:mt-4">
+                            <input type="text" placeholder="Enter your method"
+                                class="w-full h-10 border-gray-400 border-2 rounded-md p-1 ">
+                        </div>
+                    </div>
+                    <div class="md:flex items-center gap-10">
+                        <label class="md:w-32  text-right"> Technology and Tools </label>
+                        <div class="flex-1 max-md:mt-4">
+                            <input type="text" placeholder="Enter your technologie and tools"
+                                class="w-full h-10 border-gray-400 border-2 rounded-md p-1">
+                        </div>
+                    </div>
+                    <div class="md:flex items-center gap-10">
+                        <label class="md:w-32 text-right"> Research Ethics </label>
+                        <div class="flex-1 max-md:mt-4">
+                            <input type="text" placeholder="Your ethic"
+                                class="w-full h-10 border-gray-400 border-2 rounded-md p-1">
+                        </div>
+                    </div>
+                    <div class="md:flex items-start gap-10">
+                        <label class="md:w-32 text-right"> Bio </label>
+                        <div class="flex-1 max-md:mt-4">
+                            <textarea class="w-full h-32 border-gray-400 border-2 rounded-md p-1 " rows="5"
+                                placeholder="Inter your Bio"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-center gap-4 mt-16">
+                    <button type="submit"
+                        class="button lg:px-6 bg-slate-900 rounded-md h-9 text-white hover:bg-transparent hover:text-slate-900 hover:border-slate-900 hover:border-2 max-md:flex-1">
+                        Cancel</button>
+                    <button type="submit"
+                        class="button lg:px-10 bg-primary bg-slate-900 rounded-md h-9  hover:bg-transparent hover:text-slate-900 hover:border-slate-900 hover:border-2 text-white max-md:flex-1">
+                        Save <span class="ripple-overlay"></span></button>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="flex flex-col h-screen justify-center items-center">
+    <div class="max-w-sm mx-auto md:px-10 p-4 w-full">
+      <div>
+        <!-- logo -->
+        <div class="flex justify-center mb-12">
+          <!-- <img
+            src="assets/images/logo.png"
+            alt=""
+            class="w-auto h-16 shrink-0 bg-fuchsia-100 px-3 rounded-2xl p-2.5"
+          /> -->
+        </div>
+
+        <form @submit.prevent="register" class="space-y-3">
+          <input
+            class="!w-full h-10 border-gray-400 border-2 rounded-md p-1"
+            v-model="name"
+            type="text"
+            placeholder="Full name"
+            required
+          />
+          <input
+            class="!w-full h-10 border-gray-400 border-2 rounded-md p-1"
+            v-model="email"
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            class="!w-full h-10 border-gray-400 border-2 rounded-md p-1"
+            v-model="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+
+          <a href="forget" class="hidden">
+            <div class="text-sm text-right text-gray-400 py-4">Forget password</div>
+          </a>
+
+          <button
+            type="submit"
+            class="font-medium w-full hover:bg-white hover:border-slate-900 hover:border-2 hover:text-slate-900 rounded-lg bg-slate-900 py-1.5 px-4 text-white h-[38px] active:scale-[0.97] transition-all duration-150"
+          >
+            <span>Get Started</span>
+          </button>
+
+          <div class="space-x-2 text-sm text-center text-slate-400 dark:text-white/70">
+            <span>I have an account?</span>
+            <span>—</span>
+            <router-link to="/" class="text-gray-600 hover:text-gray-500">Login</router-link>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      // username: '',
+      name: '',
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    register() {
+      // Envoyer les données d'inscription au backend avec Axios
+      const userData = {
+        // username: this.username,
+        name: this.name,
+        email: this.email,
+        password: this.password
+      };
+      axios.post("https://pydev-sac.yes.bj/api/register", userData)
+        .then(response => {
+          // Gérer la réponse du backend ici
+          console.log(response.data);
+          // Rediriger l'utilisateur vers une autre page ou afficher un message de succès, etc.
+        })
+        .catch(error => {
+          // Gérer les erreurs ici
+          console.error(error);
+        });
+    }
+  }
+};
+</script>
